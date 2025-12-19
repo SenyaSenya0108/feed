51f181d765cd04bd207da23de125a9ea315f63e5
Senya Senya <senyasenya010896@gmail.com>
2025-12-19 12:41:56 +0300

    migrations for database tables, connection to RabbitMQ

--- src/cmd/cli/main.go
+++ src/cmd/cli/main.go
@@ -1,20 +1,59 @@
 package main
 
 import (
-	"context"
+	"log"
 
-	"feed/internal/cli"
-	"feed/internal/database"
+	"feed/internal/broker"
 )
 
 func main() {
-	ctx := context.Background()
-	err := database.InitDB(ctx)
+	if err := broker.Connect(); err != nil {
+		log.Panic(err)
+	}
+	ch, err := broker.Channel()
 	if err != nil {
-		return
+		log.Panic(err.Error())
+	}
+	defer func() {
+		if err := ch.Close(); err != nil {
+			log.Panic(err.Error())
+		}
+		if err := broker.Disconnect(); err != nil {
+			log.Panic(err.Error())
+		}
+	}()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	if err != nil {
+		log.Panic(err.Error())
 	}
 
-	cli.Execute()
+	msgs, err := ch.Consume(
+		q.Name,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		log.Panic(err.Error())
+	}
 
-	database.CLoseDB()
+	var forever chan struct{}
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	<-forever
 }
